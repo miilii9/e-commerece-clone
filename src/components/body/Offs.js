380be2cd68404805a2e1fd433ce3c2ec96387b5e
milosh <reza_hassani_@outlook.com>
2022-04-05 12:33:36 +0430

    fuck

--- src/components/body/Offs.js
+++ src/components/body/Offs.js
@@ -1,25 +1,23 @@
-import React from 'react'
-import './Offs.css'
-
-
+import React from 'react';
+import './Offs.css';
 
 // imgs
 import hair from '../../assets/Images/big-s-disk.jpg';
 
 export default function Offs() {
   return (
-      <div className='whol'>
-          <div className='two-pro'>
-              <img src={hair} alt='off'/>
-              <img src={hair} alt='off'/>
-          </div>
-          <div>
-              <img src={hair} alt='off' />
-          </div>
-          <div className='two-pro'>
-              <img src={hair} alt='off'/>
-              <img src={hair} alt='off'/>
-          </div>    
+    <div className="whol">
+      <div className="two-pro">
+        <img src={hair} alt="off" />
+        <img src={hair} alt="off" />
       </div>
-  )
+      <div>
+        <img src={hair} alt="off" />
+      </div>
+      <div className="two-pro">
+        <img src={hair} alt="off" />
+        <img src={hair} alt="off" />
+      </div>
+    </div>
+  );
 }
